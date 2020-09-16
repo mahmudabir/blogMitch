8082450aaccecdb3eb87eaa914b977fce5f8012e
Abir Mahmud <amabirmahmud@gmail.com>
2020-09-16 19:40:24 +0600

    19

--- account/views.py
+++ account/views.py
@@ -13,6 +13,7 @@ def registration_view(request):
             email = form.cleaned_data.get('email')
             raw_password = form.cleaned_data.get('password1')
             account = authenticate(email=email, password=raw_password)
+            login(request, account)
             return redirect('home')
         else:
             context['registration_form'] = form
@@ -60,7 +61,12 @@ def account_view(request):
     if request.POST:
         form = AccountUpdateForm(request.POST, instance=request.user)
         if form.is_valid():
+            form.initial = {
+                'email': request.POST['email'],
+                'username': request.POST['username'],
+            }
             form.save()
+            context['success_message'] = 'Updated'
     else:
         form = AccountUpdateForm(
             initial={
